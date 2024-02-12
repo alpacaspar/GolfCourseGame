925138a5078a3521680340dfa9ec52e067c5bc97
Andries <andriesdg199@gmail.com>
2024-02-12 19:54:02 +0100

    created scenes and starter scripts

---
+++ Entities/GolfBall/GolfBallMovement.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+
+var hitDirection = new()
+var wasHit = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	
+	pass
+
+
+func _on_ball_hit(givenHitDirection : Vector3):
+	hitDirection = givenHitDirection
+	wasHit = true;
+	
+	pass
