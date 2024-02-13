209b1595de96a9e8b58ab16e72ade73d611c4425
Andries <andriesdg199@gmail.com>
2024-02-13 13:30:38 +0100

    expanded golf ball and added movement

--- Entities/GolfBall/GolfBallMovement.gd
+++ Entities/GolfBall/GolfBallMovement.gd
@@ -1,17 +1,20 @@
-extends Node3D
+extends RigidBody3D
 
 
-var hitDirection = new()
-var wasHit = false
+var hitDirection
+var wasHit 
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if (Input.is_action_just_pressed("TestKey")):
+		_on_ball_hit(Vector3(1, 1, 0) * 10)
+
+
+func _physics_process(delta):
 	
 	
 	pass
@@ -21,4 +24,4 @@ func _on_ball_hit(givenHitDirection : Vector3):
 	hitDirection = givenHitDirection
 	wasHit = true;
 	
-	pass
+	apply_impulse(givenHitDirection)
