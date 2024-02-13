8246f413d14fa07d8aa577bb3a485cb38d55c6ce
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2024-02-13 13:47:35 +0100

    created finite state machine

---
+++ Common/FSM/state.gd
@@ -0,0 +1,22 @@
+class_name State
+extends Node
+
+
+func _on_enter(owner : FSM, _args = {}):
+	pass
+
+
+func _on_input(event, owner : FSM):
+	pass
+
+
+func _on_process(delta, owner : FSM):
+	pass
+
+
+func _on_physics_process(delta, owner : FSM):
+	pass
+
+
+func _on_exit(_args = {}):
+	pass
