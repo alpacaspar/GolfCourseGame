3df98a7ef0367b8f02e1cd48721bec23063e1b8b
Andries <andriesdg199@gmail.com>
2024-02-13 13:56:42 +0100

    start of ball movement

--- Entities/GolfBall/GolfBallMovement.gd
+++ Entities/GolfBall/GolfBallMovement.gd
@@ -9,19 +9,22 @@ func _ready():
 	pass
 
 
-func _process(delta):
+func _process(_delta):
 	if (Input.is_action_just_pressed("TestKey")):
 		_on_ball_hit(Vector3(1, 1, 0) * 10)
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
+	if sleeping:
+		return
 	
-	
-	pass
+	if linear_velocity.length() < 1:
+		print_debug("Runs")
+		sleeping = true
 
 
 func _on_ball_hit(givenHitDirection : Vector3):
 	hitDirection = givenHitDirection
-	wasHit = true;
+	wasHit = true
 	
 	apply_impulse(givenHitDirection)
