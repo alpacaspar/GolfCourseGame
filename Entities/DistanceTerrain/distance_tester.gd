232b8126c4dfc890ea40f78a1222b6d83f29de5d
Andries <andriesdg199@gmail.com>
2024-02-19 16:39:20 +0100

    created distance tracking scene

--- Entities/DistanceTerrain/distance_tester.gd
+++ Entities/DistanceTerrain/distance_tester.gd
@@ -5,7 +5,16 @@ extends Node3D
 
 
 func _ready():
-	for n in 250:
+	for n in 400:
+		if n < 1:
+			for cm in 10:
+				var tmp = block.instantiate() as Node3D
+				add_child(tmp)
+				tmp.global_position = Vector3(float(cm) / 10.0, 0, 0)
+				tmp.get_child(0).set_text("")
+			
+			continue
+		
 		if n < 10:
 			var tmp = block.instantiate() as Node3D
 			add_child(tmp)
@@ -18,7 +27,3 @@ func _ready():
 			add_child(tmp)
 			tmp.global_position = Vector3(n, 0, 0)
 			tmp.get_child(0).set_text(str(n) + "m")
-
-
-func _process(delta):
-	pass
