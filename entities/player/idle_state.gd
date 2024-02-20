f47c19a3fd6ebfdb595a6bf7294a1915cdaa26a1
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2024-02-20 12:29:25 +0100

    implemented input provider into player

--- entities/player/idle_state.gd
+++ entities/player/idle_state.gd
@@ -1,22 +1,22 @@
 extends State
 
 
-@export var player : CharacterBody3D
+@export var player: CharacterBody3D
 
 
-func _on_enter(_owner : FSM, _args = {}):
+func _on_enter(_owner: FSM, _args = {}):
 	pass
 
 
-func _on_input(_event, _owner : FSM):
+func _on_input(_event, _owner: FSM):
 	pass
 
 
-func _on_process(_delta, _owner : FSM):
+func _on_process(_delta, _owner: FSM):
 	pass
 
 
-func _on_physics_process(delta, _owner : FSM):
+func _on_physics_process(delta, _owner: FSM):
 	player.velocity.y = -1 if player.is_on_floor() else player.velocity.y - player.gravity * delta
 	
 	# This function already uses the delta time internally.
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -4,8 +4,10 @@ extends CharacterBody3D
 const SPEED = 5.0
 const SENSITIVITY = .5
 
-@onready var camera_pivot : Marker3D = $CameraPivot
-@onready var visuals : Node3D = $Visuals
+@onready var camera_pivot: Marker3D = $CameraPivot
+@onready var visuals: Node3D = $Visuals
+
+@export var input_provider: InputProvider
 
 @export var state_machine : FSM
 @export var golf_manager : GolfManager
@@ -14,7 +16,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 # For debugging purposes.
-func _input(_event):
+func _input(_event: InputEvent):
 	if Input.is_action_just_released("ui_cancel"):
 		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
