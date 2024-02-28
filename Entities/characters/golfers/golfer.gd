5b14c457d66a4e318289eff3fe6f79c19497de0d
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2024-02-28 09:09:46 +0100

    setup rival and golfer resources

---
+++ Entities/characters/golfers/golfer.gd
@@ -0,0 +1,18 @@
+class_name Golfer
+extends Resource
+
+
+@export var name := "Joe Schmoe"
+@export var level := 1
+@export var role := Role
+
+var power: int
+var stamina: int
+var experience: int
+
+
+func _init(golfer_name := "", golfer_level := 1):
+    name = golfer_name
+    level = golfer_level
+    power = 10 * golfer_level
+    stamina = 10 * golfer_level
