7d074d19398bbeaa91bd7ec70c6ee93de2a43543
Andries <andriesdg199@gmail.com>
2024-02-28 17:35:23 +0100

    general additives for tool

--- addons/golfer_editor/ui/golfer_customizer.gd
+++ addons/golfer_editor/ui/golfer_customizer.gd
@@ -3,6 +3,8 @@ class_name GolferCustomizer
 extends Panel
 
 
+var active: bool = false
+
 @export var golfer_resource: ResourceLoadingHandler
 @export var npc_resource: ResourceLoadingHandler
 
@@ -30,7 +32,36 @@ func _ready():
 	npc_resource._set_value()
 
 
+
+	active = true
+
+
 func _process(delta):
+	if not Engine.is_editor_hint():
+		return
+
+	if not active:
+		return
+
+	if golfer_resource.value == null:
+		save_button.disabled = true
+		load_button.disabled = true
+	else:
+		save_button.disabled = false
+		load_button.disabled = false
+
+	_check_for_errors()
+	if has_error:
+		save_button.disabled = true
+		save_as_button.disabled = true
+	else:
+		save_as_button.disabled = false
+
+
+
+func _update_resource():
+	edited_resource.golf_class = class_dropdown.get_item_text(class_dropdown.get_selected_id())
+	
 	pass
 
 
@@ -61,3 +92,22 @@ func _save_as():
 		print(result)
 	else:
 		print("Successfully saved")
+
+
+func _check_for_errors():
+	var result := ""
+
+	if npc_resource.value == null:
+		result += "NPC Resource cannot be empty"
+
+	if result == "":
+		result = "No Errors Found."
+		error_display.label_settings.font_color = Color(1.0, 1.0, 1.0, 1.0)
+		error_icon.texture = load("res://addons/npc_editor/ui/Info_icon.png")
+		has_error = false
+	else:
+		error_display.label_settings.font_color = Color(1.0, 0.3, 0.3, 1.0)
+		error_icon.texture = load("res://addons/npc_editor/ui/Error_icon.png")
+		has_error = true
+
+	error_display.text = result
