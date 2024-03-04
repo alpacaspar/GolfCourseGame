01cd79a59d97c4f5de0c10797377da443f00ce09
Andries <andriesdg199@gmail.com>
2024-03-04 16:26:34 +0100

    created plugin for sequencer

---
+++ addons/cutscene_sequencer/cutscene_sequencer.gd
@@ -0,0 +1,18 @@
+@tool
+extends EditorPlugin
+
+
+var dock
+
+
+func _enter_tree():
+	# Initialization of the plugin goes here.
+	dock = load("res://addons/cutscene_sequencer/ui/sequencer_tool.tscn").instantiate()
+	add_control_to_bottom_panel(dock, "Cutscene Sequencer")
+
+	make_bottom_panel_item_visible(dock)
+
+
+func _exit_tree():
+	remove_control_from_bottom_panel(dock)
+	
