f728b17344f59f1a09de37e1067dc583648cc492
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2024-03-14 09:47:57 +0100

    added error handling to composite node

--- common/behaviour_tree/bt_composite.gd
+++ common/behaviour_tree/bt_composite.gd
@@ -7,6 +7,7 @@ var child_nodes: Array[BTNode]
 
 
 func _ready():
+	assert(get_child_count() > 0, "BTComposite must have at least one child node.")
 	for child_node: Node in get_children():
 		if child_node is BTNode:
 			child_nodes.append(child_node)
