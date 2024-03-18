60e53c90c2022fdb4ec226725563bf4e6a2a0a8e
Nomax-Official <84581437+Nomax-Official@users.noreply.github.com>
2024-03-18 12:32:13 +0100

    Integrated Wwise (#15)
    * imported wwise addon
    
    * set up wwise integration
    
    * Added Wwise Soundbanks and added Audio Test Level
    
    * Rebase Fix: Enabled Wwise Plugin
    
    ---------
    
    Co-authored-by: CasparG00 <61011805+CasparG00@users.noreply.github.com>

---
+++ GeneratedSoundBanks/wwise_ids.gd
@@ -0,0 +1,121 @@
+class_name AK
+
+class EVENTS:
+
+	const PLAY_X_DEBUG_DISTANCE = 3502184788
+	const PLAY_X_DEBUG_CONTINUOUS_SOUND = 3634679436
+
+	const _dict = {
+		"play_x_debug_distance": PLAY_X_DEBUG_DISTANCE,
+		"play_x_debug_continuous_sound": PLAY_X_DEBUG_CONTINUOUS_SOUND
+	}
+
+class STATES:
+
+	const _dict = {}
+
+class SWITCHES:
+
+	class DEBUG_DISTANCE:
+		const GROUP = 1611159812
+
+		class SWITCH:
+			const DISTANCE_080 = 1147998661
+			const DISTANCE_055 = 1198331547
+			const DISTANCE_085 = 1147998656
+			const DISTANCE_090 = 1131221074
+			const DISTANCE_015 = 1265441999
+			const DISTANCE_045 = 1215109236
+			const DISTANCE_060 = 1181553963
+			const DISTANCE_050 = 1198331550
+			const DISTANCE_100 = 1181406900
+			const DISTANCE_065 = 1181553966
+			const DISTANCE_095 = 1131221079
+			const DISTANCE_000 = 1282219709
+			const DISTANCE_030 = 1231886820
+			const DISTANCE_005 = 1282219704
+			const DISTANCE_070 = 1164776376
+			const DISTANCE_020 = 1248664407
+			const DISTANCE_035 = 1231886817
+			const DISTANCE_025 = 1248664402
+			const DISTANCE_075 = 1164776381
+			const DISTANCE_010 = 1265441994
+			const DISTANCE_040 = 1215109233
+
+	const _dict = {
+		"debug_distance": {
+			"GROUP": 1611159812,
+			"SWITCH": {
+				"distance_080": 1147998661,
+				"distance_055": 1198331547,
+				"distance_085": 1147998656,
+				"distance_090": 1131221074,
+				"distance_015": 1265441999,
+				"distance_045": 1215109236,
+				"distance_060": 1181553963,
+				"distance_050": 1198331550,
+				"distance_100": 1181406900,
+				"distance_065": 1181553966,
+				"distance_095": 1131221079,
+				"distance_000": 1282219709,
+				"distance_030": 1231886820,
+				"distance_005": 1282219704,
+				"distance_070": 1164776376,
+				"distance_020": 1248664407,
+				"distance_035": 1231886817,
+				"distance_025": 1248664402,
+				"distance_075": 1164776381,
+				"distance_010": 1265441994,
+				"distance_040": 1215109233
+			}
+		}
+	}
+
+class GAME_PARAMETERS:
+
+	const DEBUG_DISTANCE = 1611159812
+
+	const _dict = {
+		"debug_distance": DEBUG_DISTANCE
+	}
+
+class TRIGGERS:
+
+	const _dict = {}
+
+class BANKS:
+
+	const INIT = 1355168291
+	const DEBUG_SOUNDBANK = 56103612
+
+	const _dict = {
+		"Init": INIT,
+		"debug_soundbank": DEBUG_SOUNDBANK
+	}
+
+class BUSSES:
+
+	const MASTER_AUDIO_BUS = 3803692087
+
+	const _dict = {
+		"Master Audio Bus": MASTER_AUDIO_BUS
+	}
+
+class AUX_BUSSES:
+
+	const _dict = {}
+
+class AUDIO_DEVICES:
+
+	const NO_OUTPUT = 2317455096
+	const SYSTEM = 3859886410
+
+	const _dict = {
+		"No_Output": NO_OUTPUT,
+		"System": SYSTEM
+	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {}
+
