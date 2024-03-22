02d15801e2e9804c513a8dfa2bb0fac5749100eb
Nomax <84581437+Nomax-Official@users.noreply.github.com>
2024-03-22 11:38:38 +0100

    Footstep sounds called via method calls
    Pushing this incase I can't get it working again

--- GeneratedSoundBanks/wwise_ids.gd
+++ GeneratedSoundBanks/wwise_ids.gd
@@ -2,14 +2,16 @@ class_name AK
 
 class EVENTS:
 
-	const PLAY_PLR_FOOTSTEPS = 351037488
 	const PLAY_X_DEBUG_CONTINUOUS_SOUND = 3634679436
+	const PLAY_PLR_SWING = 3408384243
 	const PLAY_X_DEBUG_DISTANCE = 3502184788
+	const PLAY_PLR_FOOTSTEP = 3735612561
 
 	const _dict = {
-		"play_plr_footsteps": PLAY_PLR_FOOTSTEPS,
 		"play_x_debug_continuous_sound": PLAY_X_DEBUG_CONTINUOUS_SOUND,
-		"play_x_debug_distance": PLAY_X_DEBUG_DISTANCE
+		"play_plr_swing": PLAY_PLR_SWING,
+		"play_x_debug_distance": PLAY_X_DEBUG_DISTANCE,
+		"play_plr_footstep": PLAY_PLR_FOOTSTEP
 	}
 
 class STATES:
@@ -22,53 +24,53 @@ class SWITCHES:
 		const GROUP = 1611159812
 
 		class SWITCH:
-			const DISTANCE_070 = 1164776376
-			const DISTANCE_060 = 1181553963
-			const DISTANCE_080 = 1147998661
-			const DISTANCE_040 = 1215109233
-			const DISTANCE_100 = 1181406900
-			const DISTANCE_055 = 1198331547
-			const DISTANCE_045 = 1215109236
-			const DISTANCE_035 = 1231886817
-			const DISTANCE_025 = 1248664402
-			const DISTANCE_065 = 1181553966
-			const DISTANCE_075 = 1164776381
-			const DISTANCE_095 = 1131221079
-			const DISTANCE_050 = 1198331550
-			const DISTANCE_090 = 1131221074
 			const DISTANCE_015 = 1265441999
+			const DISTANCE_060 = 1181553963
 			const DISTANCE_085 = 1147998656
-			const DISTANCE_005 = 1282219704
+			const DISTANCE_020 = 1248664407
+			const DISTANCE_080 = 1147998661
+			const DISTANCE_100 = 1181406900
+			const DISTANCE_075 = 1164776381
+			const DISTANCE_030 = 1231886820
+			const DISTANCE_045 = 1215109236
 			const DISTANCE_000 = 1282219709
 			const DISTANCE_010 = 1265441994
-			const DISTANCE_020 = 1248664407
-			const DISTANCE_030 = 1231886820
+			const DISTANCE_070 = 1164776376
+			const DISTANCE_035 = 1231886817
+			const DISTANCE_065 = 1181553966
+			const DISTANCE_095 = 1131221079
+			const DISTANCE_025 = 1248664402
+			const DISTANCE_040 = 1215109233
+			const DISTANCE_090 = 1131221074
+			const DISTANCE_005 = 1282219704
+			const DISTANCE_050 = 1198331550
+			const DISTANCE_055 = 1198331547
 
 	const _dict = {
 		"debug_distance": {
 			"GROUP": 1611159812,
 			"SWITCH": {
-				"distance_070": 1164776376,
-				"distance_060": 1181553963,
-				"distance_080": 1147998661,
-				"distance_040": 1215109233,
-				"distance_100": 1181406900,
-				"distance_055": 1198331547,
-				"distance_045": 1215109236,
-				"distance_035": 1231886817,
-				"distance_025": 1248664402,
-				"distance_065": 1181553966,
-				"distance_075": 1164776381,
-				"distance_095": 1131221079,
-				"distance_050": 1198331550,
-				"distance_090": 1131221074,
 				"distance_015": 1265441999,
+				"distance_060": 1181553963,
 				"distance_085": 1147998656,
-				"distance_005": 1282219704,
+				"distance_020": 1248664407,
+				"distance_080": 1147998661,
+				"distance_100": 1181406900,
+				"distance_075": 1164776381,
+				"distance_030": 1231886820,
+				"distance_045": 1215109236,
 				"distance_000": 1282219709,
 				"distance_010": 1265441994,
-				"distance_020": 1248664407,
-				"distance_030": 1231886820
+				"distance_070": 1164776376,
+				"distance_035": 1231886817,
+				"distance_065": 1181553966,
+				"distance_095": 1131221079,
+				"distance_025": 1248664402,
+				"distance_040": 1215109233,
+				"distance_090": 1131221074,
+				"distance_005": 1282219704,
+				"distance_050": 1198331550,
+				"distance_055": 1198331547
 			}
 		}
 	}
@@ -109,12 +111,12 @@ class AUX_BUSSES:
 
 class AUDIO_DEVICES:
 
-	const NO_OUTPUT = 2317455096
 	const SYSTEM = 3859886410
+	const NO_OUTPUT = 2317455096
 
 	const _dict = {
-		"No_Output": NO_OUTPUT,
-		"System": SYSTEM
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
 	}
 
 class EXTERNAL_SOURCES:
--- common/character_factory/ak_locomotion.gd
+++ common/character_factory/ak_locomotion.gd
@@ -2,10 +2,14 @@ extends Node
 
 
 func play_footstep():
-	Wwise.register_game_obj(self, "AkEvent3D")
+	Wwise.register_game_obj(self, "AkTest")
 	Wwise.post_event("play_plr_footstep", self)
 
 
 func play_swing():
-	Wwise.register_game_obj(self, "AkEvent3D")
+	Wwise.register_game_obj(self, "AkTest")
 	Wwise.post_event("play_plr_swing", self)
+	
+func play_test():
+	Wwise.register_game_obj(self, "AkTest")
+	Wwise.post_event("play_x_debug_distance", self)
