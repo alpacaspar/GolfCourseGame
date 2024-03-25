1518882a853f992920647264f99b77a832492341
Nomax <84581437+Nomax-Official@users.noreply.github.com>
2024-03-25 16:40:11 +0100

    Footstep Volume Decay Over Time
    Footsteps volume decreases over time while active and resets when the player becomes inactive

--- common/character_factory/ak_locomotion.gd
+++ common/character_factory/ak_locomotion.gd
@@ -1,10 +1,24 @@
 extends Node
 
+@onready var activity_timer : Timer = $ActivityTimer
+@onready var decay_timer : Timer = $DecayTimer
+@export var curve : Curve
+
+
 
 func play_footstep():
 	Wwise.register_game_obj(self, "AkTest")
 	Wwise.post_event("play_plr_footstep", self)
+	activity_timer_start()
 
+func activity_timer_start():
+	if activity_timer.time_left == 0:
+		Wwise.set_rtpc_value("footstep_volume", 1, self)
+		decay_timer.start()
+	activity_timer.start()
+	Wwise.set_rtpc_value("footstep_volume", curve.sample(decay_timer.time_left / decay_timer.wait_time), self)
+	# Wwise.set_rtpc_value("footstep_volume", 0, self)
+	
 
 func play_swing():
 	Wwise.register_game_obj(self, "AkTest")
@@ -13,3 +27,5 @@ func play_swing():
 func play_test():
 	Wwise.register_game_obj(self, "AkTest")
 	Wwise.post_event("play_x_debug_distance", self)
+
+
