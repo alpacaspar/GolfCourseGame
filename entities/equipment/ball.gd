4b375351358dc2f7dd5ecdd6b32abd76df378f6e
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2024-04-30 13:34:16 +0200

    fixed ball collision detection

--- entities/equipment/ball.gd
+++ entities/equipment/ball.gd
@@ -1,23 +1,25 @@
 extends RigidBody3D
 
 
-const VELOCITY_DAMAGE_THRESHOLD = 10.0
+const VELOCITY_DAMAGE_THRESHOLD = 1.0
 
 var owning_unit: Unit
 
 
-func _on_body_entered(unit: Node3D):
-    if linear_velocity.length_squared() < VELOCITY_DAMAGE_THRESHOLD ** 2:
+func _on_body_entered(body: Node) -> void:
+    if not body is Unit:
         return
 
-    if not unit is Unit:
+    if body == owning_unit:
         return
 
-    if unit == owning_unit:
+    if body.team == owning_unit.team:
+        return
+    
+    if body.linear_velocity.length_squared() < VELOCITY_DAMAGE_THRESHOLD ** 2:
         return
 
-    if unit.team == owning_unit.team:
-        return
-
-    if unit.has_method("take_damage"):
-        unit.take_damage(owning_unit.golfer_resource.power)
+    if body.has_method("take_damage"):
+        body.take_damage(owning_unit.golfer_resource.power)
+        # Allow only one hit per ball.
+        contact_monitor = false
