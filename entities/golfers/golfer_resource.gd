cc1283a089028406329144f0fde1d97fba40df64
Caspar Gelderman <caspar.gelderman@gmail.com>
2024-05-21 21:03:18 +0200

    implemented base stats for roles

--- entities/golfers/golfer_resource.gd
+++ entities/golfers/golfer_resource.gd
@@ -11,34 +11,34 @@ var power: int: set = set_power
 var stamina: int: set = set_stamina
 
 var experience: int:
-    set(value):
-        experience = max(value, 0)
+	set(value):
+		experience = max(value, 0)
 
-        while experience >= level:
-            experience -= level
-            level += 1
+		while experience >= level:
+			experience -= level
+			level += 1
 
 var bond: int: set = set_bond
 
 
 func _init(golfer_level := 1, golfer_role: Role = Role.new(), golfer_npc_resource: NPCResource = null):
-    level = golfer_level
-    role = golfer_role
-    npc_resource = golfer_npc_resource
+	level = golfer_level
+	role = golfer_role
+	npc_resource = golfer_npc_resource
 
-    power = 10
-    stamina = 100
-    experience = 0
-    bond = 0
+	power = golfer_role.base_power
+	stamina = golfer_role.base_stamina
+	experience = 0
+	bond = 0
 
 
 func set_power(new_power: int):
-    power = max(new_power, 0)
+	power = max(new_power, 0)
 
 
 func set_stamina(new_stamina: int):
-    stamina = max(new_stamina, 0)
+	stamina = max(new_stamina, 0)
 
 
 func set_bond(new_bond: int):
-    bond = clamp(new_bond, 0, 100)
+	bond = clamp(new_bond, 0, 100)
