f78d967c2f77e52cf90118082ee9313d6e472917
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2024-05-24 14:05:16 +0200

    created sensor decorator

---
+++ entities/unit/behaviour/decorator_sensor_radius.gd
@@ -0,0 +1,64 @@
+extends BTDecorator
+
+
+@export var radius := 10.0
+@export var team_filter := TeamFilter.ALL
+@export var exclude_self := false
+
+
+func _decorate(blackboard: Dictionary):
+    var result: Array[Unit]
+    
+    # Reuse the entities array if it already exists.
+    result = blackboard["entities"] if blackboard.has("entities") else []
+    result.clear()
+
+    var unit: Unit = blackboard["unit"]
+    var team: Team = self.team
+
+    var space_state := unit.get_world_3d().direct_space_state
+    var query := _get_query(unit.global_transform)
+
+    var intersections: Array[Dictionary] = space_state.intersect_shape(query)
+
+    for intersection: Dictionary in intersections:
+        var colliding_object: Node3D = intersection["collider"]
+
+        if not colliding_object is Unit:
+            continue
+
+        var intersected_unit: Unit = colliding_object as Unit
+        match team_filter:
+            TeamFilter.ALL:
+                result.append(intersected_unit)
+            TeamFilter.OPPONENT:
+                if intersected_unit.team != team:
+                    result.append(intersected_unit)
+            TeamFilter.ALLY:
+                if intersected_unit.team == team:
+                    result.append(intersected_unit)
+    
+    if exclude_self:
+        result.erase(unit)
+
+    
+    blackboard["entities"] = result
+    PhysicsServer3D.free_rid(query.shape_rid)
+
+
+func _get_query(transform: Transform3D) -> PhysicsShapeQueryParameters3D:
+    var shape_rid := PhysicsServer3D.sphere_shape_create()
+    PhysicsServer3D.shape_set_data(shape_rid, radius)
+
+    var params = PhysicsShapeQueryParameters3D.new()
+    params.shape_rid = shape_rid
+    params.transform = transform
+
+    return params
+
+
+enum TeamFilter {
+    ALL,
+    OPPONENT,
+    ALLY
+}
