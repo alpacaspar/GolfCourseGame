5aa8b5abb58e05d1de4ca12d4de978919dc3bf0a
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2024-05-24 14:04:24 +0200

    removed old behaviour scripts for refactor

--- entities/unit/behaviour/set_movement_target.gd
+++
@@ -1,9 +0,0 @@
-extends BTAction
-
-
-func _tick(blackboard: Dictionary, _delta: float) -> int:
-    var unit: Unit = blackboard["unit"]
-
-    unit.controller.set_movement_target(unit.target.global_position)
-
-    return SUCCESS
