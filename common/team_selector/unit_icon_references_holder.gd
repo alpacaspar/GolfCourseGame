789938c1bd573777d4ca89558c269aacdc198079
Andries <andriesdg199@gmail.com>
2024-05-27 22:26:55 +0200

    expanded unit_icon with empty version and callback

--- common/team_selector/unit_icon_references_holder.gd
+++ common/team_selector/unit_icon_references_holder.gd
@@ -7,7 +7,30 @@ extends Node
 @export var class_letter: Label
 @export var level_text: Label
 
+@export var button: Button
+
+var current_golfer: GolferResource = null
+var callback: Callable
+
+
+func _ready():
+	button.pressed.connect(_on_clicked)
+
 
 func set_values(resource: GolferResource):
+	current_golfer = resource
+	
 	level_text.text = str(resource.level)
-	class_letter.text = resource.role.descripive_letter
+	class_letter.text = resource.role.descriptive_letter
+
+
+func set_empty():
+	level_text.text = "X"
+	class_letter.text = ""
+	
+	icon.texture = null
+	current_golfer = null
+
+
+func _on_clicked():
+	callback.call(self)
