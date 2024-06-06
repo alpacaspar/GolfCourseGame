845a353d221f66c6c36c67319ae351b79c28a84e
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2024-06-06 16:20:02 +0200

    added function for detecting terrain material

--- entities/character/ak_locomotion.gd
+++ entities/character/ak_locomotion.gd
@@ -1,29 +1,41 @@
 extends Node3D
 
 
-@export var curve : Curve
+@export_group("footsteps")
+@export var footstep_volume_curve: Curve
+@export var terrain3d_storage: Terrain3DStorage
+@export var terrain3d_texture_list: Terrain3DTextureList
 
-@onready var activity_timer : Timer = $ActivityTimer
-@onready var decay_timer : Timer = $DecayTimer
+@onready var activity_timer: Timer = $ActivityTimer
+@onready var decay_timer: Timer = $DecayTimer
 
 
 func play_footstep():
-	Wwise.register_game_obj(self, get_name())
-	Wwise.set_3d_position(self, global_transform)
-	Wwise.post_event("play_plr_footstep", self)
-	start_activity_timer()
+    Wwise.register_game_obj(self, get_name())
+    Wwise.set_3d_position(self, global_transform)
+    Wwise.post_event("play_plr_footstep", self)
+
+    start_activity_timer()
+
+    # TODO: Play correct footsteps!
+    # print(_get_terrain_material_name(global_position))
 
 
 func start_activity_timer():
-	if activity_timer.time_left == 0:
-		Wwise.set_rtpc_value("footstep_volume", 1, self)
-		decay_timer.start()
+    if activity_timer.time_left == 0:
+        Wwise.set_rtpc_value("footstep_volume", 1, self)
+        decay_timer.start()
 
-	activity_timer.start()
-	Wwise.set_rtpc_value("footstep_volume", curve.sample(decay_timer.time_left / decay_timer.wait_time), self)
+    activity_timer.start()
+    Wwise.set_rtpc_value("footstep_volume", footstep_volume_curve.sample(decay_timer.time_left / decay_timer.wait_time), self)
 
 
 func play_swing():
-	Wwise.register_game_obj(self, get_name())
-	Wwise.set_3d_position(self, global_transform)
-	Wwise.post_event("play_plr_swing", self)
+    Wwise.register_game_obj(self, get_name())
+    Wwise.set_3d_position(self, global_transform)
+    Wwise.post_event("play_plr_swing", self)
+
+
+func _get_terrain_material_name(origin: Vector3) -> String:
+    var terrain_id := terrain3d_storage.get_texture_id(origin)
+    return terrain3d_texture_list.get_texture(roundi(terrain_id.x)).name
