326d147f4a3e2c022f838daf2b94c7fad3f8602b
Andries <andriesdg199@gmail.com>
2024-06-28 01:16:04 +0200

    expanded UI more

--- common/character_creator/tools/expanded_scroll_container.gd
+++ common/character_creator/tools/expanded_scroll_container.gd
@@ -4,15 +4,20 @@ extends ScrollContainer
 @export var show_slider: bool
 @export var slider: ScrollBar
 
+@export var slider_grab_sound: String
+@export var slider_ungrab_sounds: String
+
 var last_val = 0
 
 
 func _ready():
-	if slider != null:	
+	if slider != null:
 		if !show_slider:
 			slider.visible = false
 			get_child(1).get_child(2).visible = false
 		
+		# slider.drag_started.connect(play_slider_grab_sound)
+		# slider.drag_ended.connect(play_slider_ungrab_sound)
 		slider.value_changed.connect(_scroll)
 
 
@@ -21,9 +26,14 @@ func _process(_delta):
 		_scroll(scroll_vertical)
 
 	slider.max_value = get_child(0).size.y - size.y
+	slider.page = slider.max_value / 5
 	last_val = scroll_vertical
 
 
 func _scroll(value := 0):
 	scroll_vertical = value
 	slider.value = value
+
+
+func play_slider_grab_sound(): Wwise.post_event(slider_grab_sound, self)
+func play_slider_ungrab_sound(_value): Wwise.post_event(slider_ungrab_sounds, self)
